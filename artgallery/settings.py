f15f36a26e82a1a73a42ff3a406dc9b5c0f21c3a
KevAkaSlayer <pukman41@gmail.com>
2024-05-22 23:43:10 +0600

    :(

--- artgallery/settings.py
+++ artgallery/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
 ]
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
-    "https://art-gallery-fe.vercel.app/"  # Allow requests from your development origin
+    "https://art-gallery-fe.vercel.app"  # Allow requests from your development origin
 ]
 
 
